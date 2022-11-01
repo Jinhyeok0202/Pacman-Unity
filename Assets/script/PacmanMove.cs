68b070b292e6cfff1ac8cc2ce37deba990a8f1de
Jinhyeok0202 <dbswlsgur4321@gmail.com>
2022-11-01 12:30:32 +0900

    re

--- Assets/script/PacmanMove.cs
+++ Assets/script/PacmanMove.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Security.Cryptography;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
 
 public class PacmanMove : MonoBehaviour
 {
